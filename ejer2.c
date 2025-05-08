d81cc978f20ac62534ba159cf2355039d01825fb
AnaT1712 <anathernandeza1@gmail.com>
2025-05-08 14:21:08 -0600

    Create ejer2.c

---
+++ ejer2.c
@@ -0,0 +1,25 @@
+// utilizando una lista, capturar las 5 calificaciones de un alumno e imprimir el promedio 
+
+#include <stdio.h>
+int main()
+
+{
+    int list [5];
+    int i;
+    int prom;
+    int calif;
+    
+    for (i=0; i<5; i++) 
+    {
+        printf("introducir calif numero %d:",i+1);
+        scanf("%d", &list[i]);
+    }
+    
+    for(i=0; i<5; i++)
+    { 
+        calif= calif+list[i];
+    }
+    
+    prom= calif/5;
+    printf("El promedio de calificaciones es: %d\n", prom);
+} 
