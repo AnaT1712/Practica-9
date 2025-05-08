a1ed49a68bc9a7ee02da36f5ba4f20ddb6a621aa
AnaT1712 <anathernandeza1@gmail.com>
2025-05-08 13:55:27 -0600

    Create ejer 1.c

---
+++ ejer 1.c
@@ -0,0 +1,19 @@
+// Práctica 9
+// Hernández Atlatenco Ana Tonalli
+
+#include <stdio.h>
+int main()
+
+{
+    int list [5]={ 57,73,24,12,20};
+    int i;
+    printf("los números de la lista son:");
+    
+    for (i=0; i<5; i++) 
+    {
+        printf("%d,", list[i]);
+    }
+    
+    printf("\n");
+    return 0;
+}   
